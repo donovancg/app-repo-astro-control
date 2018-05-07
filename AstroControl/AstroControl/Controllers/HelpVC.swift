2b69a79d5b0969303fcd7d5d62254b3f6118b0ac
Donovan Campbell-Gillies <dcgillies@Franks-Mac-mini.frontierlocal.net>
2018-05-07 13:42:16 -0700

    added help page

---
+++ AstroControl/AstroControl/Controllers/HelpVC.swift
@@ -0,0 +1,20 @@
+//
+//  HelpVC.swift
+//  AstroControl
+//
+//  Created by Donovan Campbell-Gillies on 5/7/18.
+//  Copyright © 2018 CG Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class HelpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
